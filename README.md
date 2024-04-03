1b57fa2107921ef0732ce3cfd58d5ac3acdeb83a
Lenny Lorenz <lennylorenz88@gmail.com>
2024-04-03 12:20:08 +0200

    add const and begin README

---
+++ README.md
@@ -0,0 +1,7 @@
+Joke Generator :grinning:
+============
+
+:writing_hand: My first coding project, the joke generator, is an automated random supply of hilarious jokes. No matter what the occasion shall be, this program will deliver hilarious lines at any given moment.
+
+Getting started :computer:
+-----
---
+++ main.js
@@ -0,0 +1,4 @@
+const random1 = ['cow', 'man', 'bird', 'woman', 'döner' 'cat', 'football player', 'magician', 'dancer', 'bison']:
+let pronouns = '';
+const random2 = ['poops his pants', 'eats a döner', 'cries while sleeping', 'fight a bull', 'thinks of a joke', 'fixes a lightbulb', 'marries a cat', 'proposes to a girl', 'has an aggresive outburst'];
+const random3 = ['is embarassed.', 'says nothing.', 'thinks: what the hell!', 'calls a doctor.', 'jumps off of a bridge.', 'initiates a fight', 'cancels the plans for tonight']
